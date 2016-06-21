5c6fa15cd92d97de145bb7e4652b674fbb295154
George Johnston <georgejohnston13@hotmail.com>
2016-06-21 00:03:54 -0300

    \!

--- background.js
+++ background.js
@@ -12,7 +12,8 @@ chrome.browserAction.onClicked.addListener(function(tab){
 //also shiny!!
 //listen for a message from content.js, which will contain a URL which we'll use to open a new tab!
 chrome.runtime.onMessage.addListener(function(request, sender, senderResponse){
-  if(request.message === "open_new_tab"){
-    chrome.tabs.create({"url": request.url});
+    if(request.message === "open_new_tab"){
+      chrome.tabs.create({"url": request.url});
+    }
   }
-});
+);
