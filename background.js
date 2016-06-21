bbc3f04bd6ad0fb692ced6329756c34af4100424
George Johnston <orangepek0e@Georges-MBP.eastlink.ca>
2016-06-20 23:55:29 -0300

    clicking on the browser_action icon will find the first href on the current page, alert it to the user, and then open a new tab with that href as the destination! i think i have a decent idea of how writing extensions is going to go.

--- background.js
+++ background.js
@@ -8,3 +8,11 @@ chrome.browserAction.onClicked.addListener(function(tab){
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
+
+//also shiny!!
+//listen for a message from content.js, which will contain a URL which we'll use to open a new tab!
+chrome.runtime.onMessage.addListener(function(request, sender, senderResponse){
+  if(request.message === "open_new_tab"){
+    chrome.tabs.create({"url": request.url});
+  }
+});
--- content.js
+++ content.js
@@ -4,6 +4,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     var firstHref = $("a[href^='http']").eq(0).attr("href");
 
     alert("Here's the first link on this page:"+" "+firstHref );
+
+    //ooh shiny
+    //send a message to background.js containing the first href on a page, which we'll want to open in a new tab.
+    chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
   }
 }
 );
