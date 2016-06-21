0ab0cac83d8b5c20e785cb2db57b0a85a94e56e8
George Johnston <orangepek0e@Georges-MBP.eastlink.ca>
2016-06-20 23:37:01 -0300

    clicking the browser_action icon grabs the first URL on the page and alerts it to the user, instead of on page load.

--- content.js
+++ content.js
@@ -1,2 +1,9 @@
 //content.js
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  if(request.message === "clicked_browser_action"){
+    var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+    alert("Here's the first link on this page:"+" "+firstHref );
+  }
+}
+);
